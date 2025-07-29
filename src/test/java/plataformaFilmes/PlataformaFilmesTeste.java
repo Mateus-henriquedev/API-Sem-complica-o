2abc7914fbcf4dd686d91232cac66205b8df4e03
GuiOliveiraz <guilhermeoliveirayear4@gmail.com>
2025-07-29 17:20:01 -0300

    Sequencia curso de automação

---
+++ src/test/java/plataformaFilmes/PlataformaFilmesTeste.java
@@ -0,0 +1,41 @@
+package plataformaFilmes;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlataformaFilmesTeste {
+
+    @Test
+    public void validarLogin() {
+
+        RestAssured.baseURI = "http://guioliveira.lan:8080/";
+
+        String json = "{" +
+                      "   \"email\":\"aluno@email.com\"," +
+                      "   \"senha\":\"123456\"" +
+                      "}";
+
+        Response response = post(json, ContentType.JSON, "auth");
+
+
+        assertEquals(200, response.statusCode());
+        String token = response.jsonPath().get("token");
+        System.out.println(token);
+
+    }
+
+    public Response post(Object json, ContentType contentType, String endpoint){
+
+        return RestAssured.given()
+                .contentType(contentType)
+                .body(json)
+                .when()
+                .post(endpoint)
+                .thenReturn();
+    }
+
+}
